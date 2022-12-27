0b978b3fb541d5e144f57f58ff0c31a647497944
AdamVignolles <adam.vignolles@gmail.com>
2022-12-27 17:15:39 +0100

    update part 3 error 3ieme joeur

--- part_3.py
+++ part_3.py
@@ -16,31 +16,34 @@ def get_liste_joueur():
 
 # method check_joueur_online
 def check_joueur_online():
-    
-    # supprimer les joueur qui sont plus en ligne depuis plus de 60 secondes
-    for i in liste_joueur:
-        if int(liste_joueur[i] - time.time()) > 60:
-            del liste_joueur[i]
+    while True:
+        # supprimer les joueur qui sont plus en ligne depuis plus de 60 secondes
+        for i in liste_joueur:
+            if int(liste_joueur[i] - time.time()) > 60:
+                print(f"le joueur {i} n'est plus en ligne")
+                del liste_joueur[i]
 
-    return liste_joueur
 
 
 #method pour envoyer toutes les minuttes que le joueur est toujours en ligne
 def send_user_connect(connexion, name):
     while True:
+        print("send")
         connexion.sendall(f"player_online/{name}".encode("utf-8"))
         time.sleep(60)
 
 #method pour ecouter les messages du serveur
 def listen_server(connexion):
+    print("listen")
     while True:
         data = connexion.recv(1024)
         data = data.decode("utf-8")
         # thread le get liste joueur
         data = data.split("/")
+        print(data)
         if data[0] == "player_online":
             liste_joueur[data[1]] = time.time()
-        return data
+        print(liste_joueur)
 
 
 #method pour gerer la connexion et/ou le start d'un serv d'un nouveau joueur
@@ -53,28 +56,24 @@ def connect_new_user(pseudo, port=12345):
         connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         connexion.connect(('localhost', port))
         # thread et on envoi le pseudo du joueur
-        threading.Thread(target=send_user_connect(connexion, pseudo)).start()
+        threading.Thread(target=send_user_connect, args=(connexion, pseudo)).start()
         print(2)
-    except:
+    except Exception as e :
+        print(e)
         print(3)
         #creation de la socket
         connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         connexion.bind(('localhost', port))
-        connexion.listen(5)
+        connexion.listen()
         connexion, infos_connexion = connexion.accept()
-        # on envoi le pseudo du joueur
-        connexion.sendall(f"pseudo/{pseudo}".encode("utf-8"))
+        threading.Thread(target=send_user_connect, args=(connexion, pseudo)).start()
         print(4)       
 
     return connexion
 
 # essai 
 liste_joueur = {}
-pseudo = "Adam"
+pseudo = GetPseudo.get_pseudo()
 connexion = connect_new_user(pseudo)
-threading.Thread(target=listen_server(connexion)).start()
-threading.Thread(target=send_user_connect(connexion, pseudo)).start()
-while True:
-    print(get_liste_joueur())
-    time.sleep(1)
-
+threading.Thread(target=listen_server, args=(connexion,)).start()
+threading.Thread(target=check_joueur_online).start()
