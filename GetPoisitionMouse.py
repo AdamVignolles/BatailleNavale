23d300015fbb245549c52a27e1df6b5e2fe7c076
unknown <eleve@ClasseMobile-32.curie.local>
2023-02-07 18:38:31 +0100

    ;

--- GetPoisitionMouse.py
+++ GetPoisitionMouse.py
@@ -1,4 +1,5 @@
 import turtle
+import partie_graphique as gr
 
 def get_position_case(x, y):
     # On récupère la position de la case
@@ -10,7 +11,7 @@ def get_position_case(x, y):
 def get_position_mouse():
     pos = turtle.onscreenclick(lambda x, y: print((x, y)))
     if pos != None:
-        return get_position_case(pos[0], pos[1])
+        return (int(abs(float(pos[0]))), int(abs(float(pos[1]))))
     
 
 
@@ -21,4 +22,5 @@ POSE_Y_TABLEAU = 0
 POSE_X_TABLEAU = 0
 TAILLE_CASE = 50
 print(click)
+gr.grille(0, 0)
 turtle.mainloop()
